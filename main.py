1e9e8e96f421df5e8f046859cf6f74d1dfea211d
Jesca Ryanne Acosta <jesca_ryanne_acosta@dlsu.edu.ph>
2024-02-19 17:31:40 +0800

    w

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-import cv2
-import os
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
+import os
 
-data_dir = "/data"
+folder_path = "C:/Users/Jesca Ryanne Acosta/Documents/GitHub/IMAGPRO_MP1/data"
 
 resized_images = []
 
-for filename in os.listdir(data_dir):
-    img = cv2.imread(os.path.join(data_dir, filename))
-    img_resized = cv2.resize(img, (32, 32))
-    resized_images.append(img_resized)
+for filename in os.listdir(folder_path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        img = cv2.imread(os.path.join(folder_path, filename))
+        resized_img = cv2.resize(img, (32, 32))
+        resized_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+        resized_images.append(resized_img)
 
-images_array = np.array(resized_images)
+image_tensor = np.array(resized_images)
 
-plt.figure(figsize=(10, 10))
-for i in range(len(images_array)):
+plt.figure(figsize=(10, 5))
+for i in range(6):
     plt.subplot(2, 3, i+1)
-    plt.imshow(cv2.cvtColor(images_array[i], cv2.COLOR_BGR2RGB))
+    plt.imshow(image_tensor[i])
+    plt.title(f"Image {i+1}")
     plt.axis('off')
 plt.show()
 
-print("Shape of the resulting variable:", images_array.shape)
+print("Shape of the resulting variable:", image_tensor.shape)
+
+np.save("image_tensor.npy", image_tensor)
