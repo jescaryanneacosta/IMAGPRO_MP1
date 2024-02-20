c2daf4c204ffe419b691e2f4d37cd98b91694ae8
Jesca Ryanne Acosta <jesca_ryanne_acosta@dlsu.edu.ph>
2024-02-20 10:27:47 +0800

    changed the var name cuz i made iit too long oops

--- image_stats.py
+++ image_stats.py
@@ -9,8 +9,8 @@ total_pixels = first_image.shape[0] * first_image.shape[1] * first_image.shape[2
 average_pixel_per_channel = np.mean(first_image, axis=(0, 1))
 
 normalized_image = first_image / 255.0
-pixels_less_than_or_equal_to_0_5 = np.sum(normalized_image <= 0.5)
+pixels_lessequalto = np.sum(normalized_image <= 0.5)
 
 print("Total number of pixels in the first image:", total_pixels)
 print("Average pixel value per channel:", average_pixel_per_channel)
-print("Number of pixels equal to 0.5 and less than 0.5:", pixels_less_than_or_equal_to_0_5)
+print("Number of pixels equal to 0.5 and less than 0.5:", pixels_lessequalto)
